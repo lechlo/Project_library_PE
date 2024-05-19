5489836f6cc8414e7b1ac530eb8864517a9d8af6
OlegZub <oleg_zubov95@mail.ru>
2024-05-19 22:05:45 +0500

    Add files via upload

---
+++ test/test_user.py
@@ -0,0 +1,27 @@
+import unittest
+from utils import User
+
+class TestUserClass(unittest.TestCase):
+
+    def test_user_creation(self):
+        user = User("John", "Doe", "Ser", "13.04.2001", "johndoe@example.com", "студент")
+        self.assertEqual(user.name, "John")
+        self.assertEqual(user.fam, "Doe")
+        self.assertEqual(user.oth, "Ser")
+        self.assertEqual(user.date, "13.04.2001")
+        self.assertEqual(user.email, "johndoe@example.com")
+        self.assertEqual(user.status, "студент")
+
+    def test_change_password(self):
+        user = User("John", "Doe", "Ser", "13.04.2001", "johndoe@example.com", "студент")
+        user.change_password("newpassword456")
+        self.assertEqual(user.password, "newpassword456")
+
+    def test_user_borrow_book(self):
+        user = User("John", "Doe", "Ser", "13.04.2001", "johndoe@example.com", "студент")
+        book_info = ("Test Book", "Author", "1234", 5)
+        result = user.borrow_book(book_info)
+        self.assertTrue(result)  # Assuming borrow_book returns True on success
+
+if __name__ == '__main__':
+    unittest.main()
